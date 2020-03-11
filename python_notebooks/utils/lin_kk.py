c8496820ec9b24f2116675d67ff03c33c2e59fff
cjang000 <cjang@wildcatdiscovery.com>
2020-03-11 09:31:23 -0700

    func done

--- python_notebooks/utils/lin_kk.py
+++ python_notebooks/utils/lin_kk.py
@@ -4662,17 +4662,6 @@ def KK_timeconst(w, num_RC):
 
 
 
-def KK_errorfunc_2(w, re, im, num_RC, t_values):
-    '''
-    test
-    '''
-    if num_RC == 51:
-        re_fit = curve_fit(lambda w, Rs, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39, R40, R41, R42, R43, R44, R45, R46, R47, R48, R49, R50, R51: KK_RC51_fit(w, Rs, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39, R40, R41, R42, R43, R44, R45, R46, R47, R48, R49, R50, R51, t_values), w, re)[0]
-#        im_fit = curve_fit(lambda w, Rs, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39, R40, R41, R42, R43, R44, R45, R46, R47, R48, R49, R50, R51: KK_RC51_fit(w, Rs, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16, R17, R18, R19, R20, R21, R22, R23, R24, R25, R26, R27, R28, R29, R30, R31, R32, R33, R34, R35, R36, R37, R38, R39, R40, R41, R42, R43, R44, R45, R46, R47, R48, R49, R50, R51, t_values)[1], w, im)[0]
-    else:
-        print('error in this crap')
-    return re_fit#, im_fit
-
 
 def KK_errorfunc(params, w, re, im, num_RC, weight_func, t_values):
     '''
